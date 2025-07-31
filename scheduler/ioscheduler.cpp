3f79b0d08be4833cd32bdbc86f6ff706cc2a217c
TamamoLoN <1016052306@qq.com>
2025-07-31 14:29:34 +0800

    feat:新增io调度器框架

---
+++ scheduler/ioscheduler.cpp
@@ -0,0 +1,381 @@
+#include "scheduler/ioscheduler.h"
+
+namespace lon
+{
+namespace scheduler
+{
+IOScheduler::IOScheduler(size_t threads_count, bool use_caller, std::string name,
+                         size_t fiber_stack_size)
+    : Scheduler(threads_count, use_caller, name, fiber_stack_size)
+{
+    memset(m_notify_pipe_fd, 0, sizeof(m_notify_pipe_fd));
+    m_epoll_fd = epoll_create(5000);
+    LON_ASSERT(m_epoll_fd > 0);
+
+    int rt = pipe(m_notify_pipe_fd);
+    LON_ASSERT(!rt);
+
+    epoll_event event;
+    memset(&event, 0, sizeof(event));
+    event.events  = EPOLLIN | EPOLLET;
+    event.data.fd = m_notify_pipe_fd[0];
+
+    rt = fcntl(m_notify_pipe_fd[0], F_SETFL, O_NONBLOCK);
+    LON_ASSERT(!rt);
+
+    rt = epoll_ctl(m_epoll_fd, EPOLL_CTL_ADD, m_notify_pipe_fd[0], &event);
+    LON_ASSERT(!rt);
+
+    contextResize(32);
+
+    start();
+}
+
+IOScheduler::~IOScheduler()
+{
+    stop();
+    ::close(m_epoll_fd);
+    ::close(m_notify_pipe_fd[0]);
+    ::close(m_notify_pipe_fd[1]);
+
+    for (auto &it : m_fd_contexts)
+    {
+        delete it;
+        it = nullptr;
+    }
+}
+
+uint8_t IOScheduler::addEvent(int fd, Event event, std::function<void()> cb)
+{
+    //保证fd即是索引
+    FdContext *fd_ctx = nullptr;
+    MutexType::RdLock rlock(m_mutex);
+    if (m_fd_contexts.size() > fd)
+    {
+        fd_ctx = m_fd_contexts[fd];
+        rlock.unlock();
+    }
+    else
+    {
+        rlock.unlock();
+        MutexType::WrLock wlock(m_mutex);
+        contextResize(m_fd_contexts.size() * 1.5f);
+        fd_ctx = m_fd_contexts[fd];
+    }
+    FdContext::MutexType::Lock fd_ctx_lock(fd_ctx->mutex);
+    // 一个句柄一般不会重复加同一个事件， 可能是两个不同的线程在操控同一个句柄添加事件
+    if (fd_ctx->event & event)
+    {
+        LON_ERROR(LON_LOG_ROOT) << "can not add same event ,fd=" << fd << " event=" << event
+                                << " fd_ctx->event=" << fd_ctx->event;
+        return -1;
+    }
+
+    int op = fd_ctx->event != 0 ? EPOLL_CTL_MOD : EPOLL_CTL_ADD;
+    epoll_event epevent;
+    memset(&epevent, 0, sizeof(epevent));
+    epevent.events   = EPOLLET | fd_ctx->event | event;
+    epevent.data.ptr = fd_ctx;
+    int ret          = epoll_ctl(m_epoll_fd, op, fd, &epevent);
+    if (ret != 0)
+    {
+        LON_ERROR(LON_LOG_ROOT) << "epoll_ctl error ,m_epoll_fd=" << m_epoll_fd << " op=" << op
+                                << " fd=" << fd << " epevent.events=" << epevent.events
+                                << " errno=" << errno << ": " << strerror(errno);
+        return -1;
+    }
+    ++m_waitting_events_count;
+    fd_ctx->event   = (Event)(fd_ctx->event | event);
+    auto &event_ctx = fd_ctx->getContext(event);
+    LON_ASSERT(!event_ctx.cb && !event_ctx.fiber && !event_ctx.scheduler);
+    event_ctx.scheduler = Scheduler::getThis();
+    if (cb)
+    {
+        event_ctx.cb.swap(cb);
+    }
+    else
+    {
+        event_ctx.fiber = fiber::Fiber::getThis();
+        LON_ASSERT(event_ctx.fiber->getState() == fiber::Fiber::EXEC);
+    }
+
+    return 0;
+}
+
+bool IOScheduler::delEvent(int fd, Event event)
+{
+    MutexType::RdLock rlock(m_mutex);
+    if (m_fd_contexts.size() <= fd)
+    {
+        return false;
+    }
+    FdContext *fd_ctx = m_fd_contexts[fd];
+    rlock.unlock();
+
+    FdContext::MutexType::Lock fd_ctx_lock(fd_ctx->mutex);
+    if (!(fd_ctx->event & event))
+    {
+        return false;
+    }
+    Event new_event = (Event)(fd_ctx->event & ~(event));
+    int op          = new_event != 0 ? EPOLL_CTL_MOD : EPOLL_CTL_DEL;
+    epoll_event epevent;
+    memset(&epevent, 0, sizeof(epevent));
+    epevent.events   = EPOLLET | new_event;
+    epevent.data.ptr = fd_ctx;
+    int ret          = epoll_ctl(m_epoll_fd, op, fd, &epevent);
+    if (ret != 0)
+    {
+        LON_ERROR(LON_LOG_ROOT) << "epoll_ctl error ,m_epoll_fd=" << m_epoll_fd << " op=" << op
+                                << " fd=" << fd << " epevent.events=" << epevent.events
+                                << " errno=" << errno << ": " << strerror(errno);
+        return false;
+    }
+    --m_waitting_events_count;
+    fd_ctx->event   = new_event;
+    auto &event_ctx = fd_ctx->getContext(event);
+    fd_ctx->resetContext(event_ctx);
+
+    return true;
+}
+
+bool IOScheduler::cancelEvent(int fd, Event event)
+{
+    MutexType::RdLock rlock(m_mutex);
+    if (m_fd_contexts.size() <= fd)
+    {
+        return false;
+    }
+    FdContext *fd_ctx = m_fd_contexts[fd];
+    rlock.unlock();
+
+    FdContext::MutexType::Lock fd_ctx_lock(fd_ctx->mutex);
+    if (!(fd_ctx->event & event))
+    {
+        return false;
+    }
+    Event new_event = (Event)(fd_ctx->event & ~(event));
+    int op          = new_event != 0 ? EPOLL_CTL_MOD : EPOLL_CTL_DEL;
+    epoll_event epevent;
+    memset(&epevent, 0, sizeof(epevent));
+    epevent.events   = EPOLLET | new_event;
+    epevent.data.ptr = fd_ctx;
+    int ret          = epoll_ctl(m_epoll_fd, op, fd, &epevent);
+    if (ret != 0)
+    {
+        LON_ERROR(LON_LOG_ROOT) << "epoll_ctl error ,m_epoll_fd=" << m_epoll_fd << " op=" << op
+                                << " fd=" << fd << " epevent.events=" << epevent.events
+                                << " errno=" << errno << ": " << strerror(errno);
+        return false;
+    }
+    fd_ctx->triggerEvent(event);
+    --m_waitting_events_count;
+
+    return true;
+}
+
+bool IOScheduler::cancelAll(int fd)
+{
+    MutexType::RdLock rlock(m_mutex);
+    if (m_fd_contexts.size() <= fd)
+    {
+        return false;
+    }
+    FdContext *fd_ctx = m_fd_contexts[fd];
+    rlock.unlock();
+
+    FdContext::MutexType::Lock fd_ctx_lock(fd_ctx->mutex);
+    if (!(fd_ctx->event))
+    {
+        return false;
+    }
+    int op = EPOLL_CTL_DEL;
+    epoll_event epevent;
+    memset(&epevent, 0, sizeof(epevent));
+    epevent.events   = 0;
+    epevent.data.ptr = fd_ctx;
+    int ret          = epoll_ctl(m_epoll_fd, op, fd, &epevent);
+    if (ret != 0)
+    {
+        LON_ERROR(LON_LOG_ROOT) << "epoll_ctl error ,m_epoll_fd=" << m_epoll_fd << " op=" << op
+                                << " fd=" << fd << " epevent.events=" << epevent.events
+                                << " errno=" << errno << ": " << strerror(errno);
+        return false;
+    }
+    if (fd_ctx->event & Event::READ)
+    {
+        fd_ctx->triggerEvent(Event::READ);
+        --m_waitting_events_count;
+    }
+    if (fd_ctx->event & Event::WRITE)
+    {
+        fd_ctx->triggerEvent(Event::WRITE);
+        --m_waitting_events_count;
+    }
+    LON_ASSERT(fd_ctx->event == Event::NONE);
+
+    return true;
+} // namespace scheduler
+
+void IOScheduler::notify()
+{
+    if (!hasIdleThreads())
+    {
+        return;
+    }
+    int ret = write(m_notify_pipe_fd[1], "T", 1);
+    LON_ASSERT(ret == 1);
+}
+
+bool IOScheduler::stopping() { return Scheduler::stopping() && m_waitting_events_count == 0; }
+
+void IOScheduler::idle()
+{
+    epoll_event *events = new epoll_event[64]();
+    std::shared_ptr<epoll_event> shared_events(events, [](epoll_event *ptr) {
+        delete[] ptr;
+        ptr = nullptr;
+    });
+
+    while (true)
+    {
+        int ret = 0;
+        if (stopping())
+        {
+            LON_INFO(LON_LOG_ROOT) << "ioscheduler[" << getName() << "] idle stopping exit";
+            break;
+        }
+
+        do
+        {
+            static const int MAX_TIMEOUT = 5000; // ms
+            ret                          = epoll_wait(m_epoll_fd, events, 64, MAX_TIMEOUT);
+            if (ret < 0 && errno == EINTR)
+            {
+            }
+            else
+            {
+                break;
+            }
+        } while (true);
+
+        for (int cnt = 0; cnt < ret; cnt++)
+        {
+            epoll_event &event = events[cnt];
+            if (event.data.fd == m_notify_pipe_fd[0])
+            {
+                uint8_t msg = 0;
+                while (read(m_notify_pipe_fd[0], &msg, 1) == 1)
+                {
+                }
+                continue;
+            }
+            FdContext *fd_ctx = (FdContext *)event.data.ptr;
+            FdContext::MutexType::Lock fd_ctx_lock(fd_ctx->mutex);
+            if (event.events & (EPOLLERR | EPOLLHUP))
+            {
+                event.events |= (EPOLLIN | EPOLLOUT);
+            }
+            int real_event = Event::NONE;
+            if (event.events & EPOLLIN)
+            {
+                real_event |= Event::READ;
+            }
+            if (event.events & EPOLLOUT)
+            {
+                real_event |= Event::WRITE;
+            }
+
+            if ((fd_ctx->event & real_event) == Event::NONE)
+            {
+                continue;
+            }
+            int left_event = (fd_ctx->event & ~real_event);
+            int op         = left_event ? EPOLL_CTL_MOD : EPOLL_CTL_DEL;
+            event.events   = EPOLLET | left_event;
+
+            int ret_epoll_ctl = epoll_ctl(m_epoll_fd, op, fd_ctx->fd, &event);
+            if (ret_epoll_ctl)
+            {
+                LON_ERROR(LON_LOG_ROOT)
+                    << "epoll_ctl error ,m_epoll_fd=" << m_epoll_fd << " op=" << op
+                    << " fd_ctx->fd=" << fd_ctx->fd << " event.events=" << event.events
+                    << " errno=" << errno << ": " << strerror(errno);
+                continue;
+            }
+
+            if (real_event & Event::READ)
+            {
+                fd_ctx->triggerEvent(Event::READ);
+                --m_waitting_events_count;
+            }
+            if (real_event & Event::WRITE)
+            {
+                fd_ctx->triggerEvent(Event::WRITE);
+                --m_waitting_events_count;
+            }
+        }
+
+        auto &&cur = std::move(fiber::Fiber::getThis());
+        cur->swapOut(Scheduler::getMainFiber());
+    }
+}
+
+void IOScheduler::contextResize(size_t size)
+{
+    m_fd_contexts.resize(size);
+    for (int cnt = 0; cnt < m_fd_contexts.size(); cnt++)
+    {
+        m_fd_contexts[cnt]     = new FdContext;
+        m_fd_contexts[cnt]->fd = cnt;
+    }
+}
+
+//静态方法
+IOScheduler *IOScheduler::getThis() { return dynamic_cast<IOScheduler *>(Scheduler::getThis()); }
+
+IOScheduler::FdContext::EventContext &IOScheduler::FdContext::getContext(Event event)
+{
+    switch (event)
+    {
+    case Event::READ:
+        return r_event;
+        break;
+    case Event::WRITE:
+        return w_event;
+        break;
+    default:
+        LON_ASSERT_(false, "get context error");
+        break;
+    }
+}
+
+void IOScheduler::FdContext::resetContext(EventContext &ctx)
+{
+    ctx.cb = nullptr;
+    ctx.fiber.reset();
+    ctx.scheduler = nullptr;
+}
+
+void IOScheduler::FdContext::triggerEvent(Event event)
+{
+    LON_ASSERT(event & this->event);
+    this->event = (Event)((~event) & this->event);
+    auto &ctx   = getContext(event);
+    if (ctx.cb)
+    {
+        ctx.scheduler->schedule(&ctx.cb);
+    }
+    else if (ctx.fiber)
+    {
+        ctx.scheduler->schedule(&ctx.fiber);
+    }
+    else
+    {
+        LON_ASSERT_(false, "error ctx");
+    }
+    ctx.scheduler = nullptr;
+}
+
+} // namespace scheduler
+} // namespace lon
---
+++ scheduler/ioscheduler.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "scheduler/scheduler.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/epoll.h>
+
+namespace lon
+{
+namespace scheduler
+{
+class IOScheduler : public Scheduler
+{
+  public:
+    enum Event
+    {
+        NONE  = 0x00,
+        READ  = 0x01,
+        WRITE = 0x04
+    };
+
+  public:
+    using Ptr       = std::shared_ptr<IOScheduler>;
+    using MutexType = thread::RWMutex;
+    IOScheduler(size_t threads_count = 1, bool use_caller = true, std::string name = "",
+                size_t fiber_stack_size = 1024 * 1024);
+    ~IOScheduler();
+
+    // 0: success -1: error
+    uint8_t addEvent(int fd, Event event, std::function<void()> cb = nullptr);
+    bool delEvent(int fd, Event event);
+    bool cancelEvent(int fd, Event event);
+    bool cancelAll(int fd);
+
+  public:
+    static IOScheduler *getThis();
+
+  protected:
+    void notify() override;
+    bool stopping() override;
+    void idle() override;
+
+    void contextResize(size_t size);
+
+  private:
+    struct FdContext
+    {
+        using MutexType = thread::Mutex;
+        struct EventContext
+        {
+            Scheduler *scheduler     = nullptr; //事件执行的scheduler
+            fiber::Fiber::Ptr fiber  = nullptr; //事件协程
+            std::function<void()> cb = nullptr; //事件回调函数
+        };
+        EventContext &getContext(Event event);
+        void resetContext(EventContext &ctx);
+        void triggerEvent(Event event);
+
+        EventContext r_event; //写事件
+        EventContext w_event; //读事件
+        int fd;               //事件关联句柄
+        Event event;          //当前注册事件类型
+        FdContext::MutexType mutex;
+    };
+
+  private:
+    int m_epoll_fd;
+    int m_notify_pipe_fd[2];
+    std::atomic<size_t> m_waitting_events_count;
+    MutexType m_mutex;
+    std::vector<FdContext *> m_fd_contexts;
+};
+} // namespace scheduler
+} // namespace lon
