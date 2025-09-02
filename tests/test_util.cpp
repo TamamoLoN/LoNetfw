adf545e1c8c7d8af357e6a57c60ce0e444ce1720
TamamoLoN <1016052306@qq.com>
2025-09-02 14:27:23 +0800

    mod

--- tests/test_util.cpp
+++ tests/test_util.cpp
@@ -1,5 +1,29 @@
 #include "lonetfw/lonetfw.h"
 
+void test_parse_log_format()
+{
+    auto res =
+        lon::util::formatParser("%d{%Y-%m-%d %H:%M:%S}中文%T%t%T%N%T%F%T[%p]%T[%c]%T%f:%l%T%m%n");
+    for (const auto &i : res)
+    {
+        for (const auto &j : i)
+        {
+            std::cout << j.first << ":" << (int)j.second << "\n";
+        }
+    }
+}
+
+void test_lexical_cast()
+{
+    auto vec = std::vector<std::map<std::string, int>>{
+        {{"a", 1}, {"b", 2}},
+        {{"c", 3}, {"d", 4}},
+    };
+
+    LON_INFO(LON_LOG_NAME("root"))
+        << lon::util::LexicalCast<std::string, std::vector<std::map<std::string, int>>>()(vec);
+}
+
 void test_backtrace()
 {
     try
@@ -18,6 +42,8 @@ int main(int argc, char const *argv[])
 {
     lon::config::Config::parseFromYaml("./.config/log.yaml");
     // test_backtrace();
-    test_assert();
+    // test_assert();
+    test_parse_log_format();
+    test_lexical_cast();
     return 0;
 }
